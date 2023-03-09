c7ee6a6861deb33553b033654a89b32df6e3f172
kadima <williamsvgw@gmail.com>
2023-03-09 19:56:42 +0100

    Created a Person class

---
+++ prac.php
@@ -0,0 +1,27 @@
+<?php
+
+    class Personal{
+
+        public $name;
+        public $age;
+
+        function __construct($name,$age){
+
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        public function callName(){
+
+            return $this->name;
+        }
+
+    }
+
+
+    $obj = new Personal('kadima',22);
+
+    echo $obj->callName();
+
+
+?>
